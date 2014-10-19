b93151ad71eb9b385594dde62cd04578bfdf9213
Marcel Loevenich <marcel.loevenich@online.de>
2014-10-19 12:14:22 +0200

    added game state manager

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,11 +1,15 @@
 package main;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
+
+import GameState.GameStateManager;
 
 public class GamePanel extends JPanel implements Runnable, KeyListener{
 	
@@ -24,6 +28,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 	private BufferedImage image;
 	private Graphics2D g;
 	
+	//game state manager
+	private GameStateManager gsm;
+	
 	//constructor
 	public GamePanel(){
 		super();
@@ -41,10 +48,71 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
 		}
 	}
 	
+	//initialise
+	private void init(){
+		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+		g = (Graphics2D) image.getGraphics();
+		running = true;
+		
+		gsm = new GameStateManager();
+	}
+	
 	public void run(){
+		init();
+		
+		long start;
+		long elapsed;
+		long wait;
+		
+		//game loop
+		while(running){
+			
+			start = System.nanoTime();
+			
+			update();
+			draw();
+			drawToScreen();
+			
+			elapsed = System.nanoTime() - start;
+			
+			wait= targetTime - elapsed / 1000000;
+			if(wait < 0){
+				wait = 5;
+			}
+			
+			try{
+				Thread.sleep(wait);
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	private void update(){
+		gsm.update();
+	}
+	
+	private void draw(){
+		gsm.draw(g);
+	}
+	
+	private void drawToScreen(){
+		Graphics g2 = getGraphics();
+		g2.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		g2.dispose();
+	}
+	
+	public void keyTyped (KeyEvent key){
 		
 	}
 	
-	public void key  //11,20
+	public void keyPressed (KeyEvent key){
+		gsm.keyPressed(key.getKeyCode());
+	}
+
+	public void keyReleased (KeyEvent key){
+		gsm.update();
+	}
 	
 }
